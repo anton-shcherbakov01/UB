578f03804234b796bca224423b547c6a6f5a45be
anton-shcherbakov01 <anton.sherbakov.01@gmail.com>
2026-01-22 23:48:59 +0300

    fix bugs

--- backend/tasks/__init__.py
+++ backend/tasks/__init__.py
@@ -1,14 +1,18 @@
 from .bidder import bidder_producer_task, bidder_consumer_task
-from .finance import sync_financial_reports, train_forecasting_models
+from .finance import sync_financial_reports, train_forecasting_models, sync_product_metadata
 from .seo import analyze_reviews_task, generate_seo_task, cluster_keywords_task, check_seo_position_task
 from .monitoring import parse_and_save_sku, update_all_monitored_items, check_new_orders
 from .utils import get_status
+from .supply import sync_supply_data_task
+# Добавлен импорт новой задачи
+from .price_control import check_price_alerts
 
 __all__ = [
     "bidder_producer_task",
     "bidder_consumer_task",
     "sync_financial_reports",
     "train_forecasting_models",
+    "sync_product_metadata",
     "analyze_reviews_task",
     "generate_seo_task",
     "cluster_keywords_task",
@@ -16,5 +20,7 @@ __all__ = [
     "parse_and_save_sku",
     "update_all_monitored_items",
     "check_new_orders",
-    "get_status"
+    "get_status",
+    "sync_supply_data_task",
+    "check_price_alerts" # <--- Добавлено в список экспорта
 ]
